8afe310d2ed7e0d715211a14005a0bfcda1c2c1d
Danilo_daChaga <danillo.florestal@gmail.com>
2022-08-01 16:48:38 -0300

    01.ago.22

---
+++ Ideias De Projetos Python Intermediários/ideias_de_ProjetosPythonIntermediários.py
@@ -0,0 +1,56 @@
+''' 7 Ideias de Projetos Python Intermediários
+link:       https://www.youtube.com/watch?v=N0zzcRRevoY
+
+1ª categoria : Produtos Web / site Portfólio de serviços
+Bibliotecas sugeridas: django  /  Flask
+
+2ª categoria : Sistema de cadastro de usuário
+bib:   django / Flask
+ 
+3ª categoria :  criar APIs
+API Rest  
+exemplos:
+- API do Climatempo: https://advisor.climatempo.com.br
+- API do Pokemon: https://pokeapi.co
+
+API GraphQL
+ex.
+- AniList: https://anilist.gitbook.io/anilist-ap...
+- Countries: https://github.com/trevorblades/count...
+
+
+4ª categoria :  Web Scraping  "extrair informação de qqer site" ele faz filtragem e garimpagem de dados
+bibliotecas:  Selenium / BeautifulSoup
+
+exemplo:    sistema de alerta de envio sobre o rastreio de um determinado produto
+
+5ª categoria:   AUTOMAÇÃO
+biblioteca: Selenium /
+
+bot , robô
+preeenchimento automáticos de dados
+download automático de um determinado arquivo
+automação de redes sociais / criar postagens automáticas 
+
+6ª categoria: Desenvolvimento de GUI / Interfaces
+biblioteca: PySimpleGUI
+
+Ex.: telas para scripts existentes (dar uma tela para um código que você criou)
+Bloco de notas
+To do List
+Sistema de Loguin
+Criar interfaces para os seus sistemas
+
+
+7ª categoria: Geradores de Informações
+bibliotecas: pygame
+Gerador de títulos (de filme, de música, de desenho, de anime, de histórias)
+Geradores de perfil falso 
+Geradores de playlist
+Criar Jogos (ping pong, tetris)
+
+Criação de Jogos:
+- PyGame: https://www.pygame.org/wiki/GettingSt...
+- Ideias de jogos: https://inventwithpython.com/blog/201...
+
+'''
