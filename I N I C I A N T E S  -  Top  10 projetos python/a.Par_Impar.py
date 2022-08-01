2ec110756572c3e573cfe056a0efdfde4051fbf4
Danilo_daChaga <danillo.florestal@gmail.com>
2022-08-01 20:56:48 -0300

    01/08/2022'

--- I N I C I A N T E S  -  Top  10 projetos python/a.Par_Impar.py
+++ I N I C I A N T E S  -  Top  10 projetos python/a.Par_Impar.py
@@ -4,32 +4,37 @@ import PySimpleGUI as sg
 
 class ListaParImpar:
     def __init__(self):
-        
+        #Usuário Input de dados
+        self.minimo = int(input('digite o valor mínimo da lista: '))
+        self.maximo = int(input('digite o valor máximo da lista: '))
+                        
+    def Iniciar(self):
         #Layout
         self.layout = [
             [sg.Text('Valor mínimo:'),sg.Input()],
             [sg.Text('Valor máximo:'),sg.Input()],
-            [sg.Button('Gerar lista')]
-            [sg.Output(size=(100,100))]
+            [sg.Button('Gerar lista'), sg.Button('Exit')]
             ]
-        
-    def Iniciar(self):
-        #Usuário Input de dados
-        self.minimo = int(input('digite o valor mínimo da lista: '))
-        self.maximo = int(input('digite o valor máximo da lista: '))
         #janela
-        self.janela = sg.Window('Lista Par / ímpar', size=(280,280), layout= self.layout)
+        self.janela = sg.Window('Lista Par / ímpar', layout= self.layout)
         #ler os dados da tela
         self.eventos, self.valores = self.janela.Read()
 
     def ListaParImpar(self):
-        for num in range( self.minimo, self.maximo+1):
-            if num % 2 == 0:
-                print("= nº Par", self.num)
-            continue
-            print("= nº ímpar", self.num)
-        print(ListaParImpar)    
+        try:
+            while True:
+                for num in range( self.minimo, self.maximo+1):
+                    if num % 2 == 0:
+                        print("= nº Par", self.num)
+                        continue
+                    print("= nº ímpar", self.num)
+                    if self.eventos == sg.WIN_CLOSED or self.eventos == 'Exit':
+                        break
+        except:
+            print('Favor digitar apenas números!')
+            self.Iniciar()        
 
+        self.Window.close()
 tela = ListaParImpar()
 tela.Iniciar()
 
